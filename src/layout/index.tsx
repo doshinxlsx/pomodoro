b6526609f010012b972e61aef7f0a3ae52c335b3
Dominik Orincsai <dominik.orincsai@wecanconsulting.eu>
2022-09-21 20:31:17 +0200

    faet(layout): init layout

---
+++ src/layout/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { LayoutContainer } from './styles'
+
+const Layout = () => {
+  return (
+    <LayoutContainer>
+      <h2>Layout</h2>
+    </LayoutContainer>
+  )
+}
+
+export default Layout
