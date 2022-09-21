7cd68d35f46a89fabb6ff3899f83e76f00511bf7
Dominik Orincsai <dominik.orincsai@wecanconsulting.eu>
2022-09-21 21:56:10 +0200

    feat(countdown): implemented countdown component with temp style

--- src/pages/home/components/countdown/index.tsx
+++ src/pages/home/components/countdown/index.tsx
@@ -1,10 +1,61 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { CycleContext } from '../../../../context/CycleContext'
 import { CountdownContainer } from './styles'
+import { differenceInSeconds } from 'date-fns'
 
 const Countdown = () => {
+  const {
+    activeCycle,
+    activeCycleId,
+    markCurrentCycleAsFinished,
+    amountSecondsPassed,
+    setSecondsPassed,
+  } = useContext(CycleContext)
+
+  const totalSeconds = activeCycle ? activeCycle?.minutesAmount * 60 : 0
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
+
+  const minutesAmount = Math.floor(currentSeconds / 60)
+  const secondsAmount = currentSeconds % 60
+
+  const minutes = String(minutesAmount).padStart(2, '0')
+  const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds} - Timer Pomodoro`
+    }
+  }, [activeCycle, minutes, seconds])
+
+  useEffect(() => {
+    let interval: number
+
+    if (activeCycle) {
+      interval = setInterval(() => {
+        const secondsDifference = differenceInSeconds(new Date(), new Date(activeCycle.startDate))
+
+        if (secondsDifference >= totalSeconds) {
+          markCurrentCycleAsFinished()
+          document.title = 'Timer Pomodoro'
+          clearInterval(interval)
+        } else {
+          setSecondsPassed(secondsDifference)
+        }
+      }, 1000)
+    }
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [activeCycle, totalSeconds, activeCycleId, markCurrentCycleAsFinished, setSecondsPassed])
+
   return (
     <CountdownContainer>
-      <span>Countdown</span>
+      <Minutes>Countdown</Minutes>
+      <Minutes></Minutes>
+      <Separator>:</Separator>
+      <Seconds></Seconds>
+      <Seconds></Seconds>
     </CountdownContainer>
   )
 }
--- src/pages/home/components/countdown/styles.ts
+++ src/pages/home/components/countdown/styles.ts
@@ -1,3 +1,45 @@
 import styled from 'styled-components'
 
-export const CountdownContainer = styled.div``
+export const CountdownContainer = styled.div`
+  font-family: 'Roboto Mono', monospace;
+  font-size: 10rem;
+  line-height: 8rem;
+  color: white;
+  transition: ease-in-out 0.2s;
+  display: flex;
+  gap: 1rem;
+
+  span {
+    background: black;
+    padding: 2rem 1rem;
+    border-radius: 8px;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 5rem;
+    line-height: 2rem;
+    transition: ease-in-out 0.2s;
+
+    span {
+      padding: 1rem 0;
+      background: transparent;
+    }
+  }
+`
+
+export const Separator = styled.div`
+  padding: 2rem 0;
+  color: white;
+  width: 4rem;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+
+  @media (max-width: 768px) {
+    width: 2rem;
+    padding: 0.875rem 0;
+  }
+`
+
+export const Minutes = styled.span``
+export const Seconds = styled.span``
