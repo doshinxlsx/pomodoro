d638ac8bfe9c008f9dc55d5599b407088336c2f6
Dominik Orincsai <dominik.orincsai@wecanconsulting.eu>
2022-09-21 20:28:10 +0200

    feat(header): header wip

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { HeaderContainer, HeaderLeftContainer, HeaderRightContainer } from './styles'
+import { NavLink } from 'react-router-dom'
+import { Timer, Scroll } from 'phosphor-react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <HeaderContainer>
+      <HeaderLeftContainer>
+        <p>Logo</p> {/* TODO: Add logo */}
+      </HeaderLeftContainer>
+      <HeaderRightContainer>
+        <NavLink to='/' title='Timer'>
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to='/history' title='History'>
+          <Scroll size={24} />
+        </NavLink>
+      </HeaderRightContainer>
+    </HeaderContainer>
   )
 }
 
--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -1,1 +1,9 @@
-export default {};
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  display: flex;
+`
+
+export const HeaderLeftContainer = styled.div``
+
+export const HeaderRightContainer = styled.div``
